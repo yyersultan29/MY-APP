7854d4f57f4db8f7db787df1b250c5678e5c2493
yersultan <180103230@stu.sdu.edu.kz>
2023-10-10 18:57:30 +0600

    typeracer car moving done

--- src/components/TypeRacer/TypeRacer.tsx
+++ src/components/TypeRacer/TypeRacer.tsx
@@ -4,12 +4,15 @@ import { words } from "./data"
 import "./TypeRacer.css";
 import { textMapper } from "./mapper";
 import { Alert } from "./Alert";
+import carSrc from "./blueCar.png";
 
 let currentIndex = -1;
 export const TypeRacer = () => {
 
   const [open, setOpen] = useState(false);
+
   const [correct, setCorrect] = useState(0);
+
   const [lettersList, setLettersList] = useState(() => textMapper(words));
 
   const handleRestart = useCallback(() => {
@@ -32,40 +35,61 @@ export const TypeRacer = () => {
 
       let copy = [...prev];
       console.log(currentIndex);
+
       if (e.key === copy[currentIndex].letter) {
         copy[currentIndex] = {
           className: "correct",
           letter: copy[currentIndex].letter
         };
         setCorrect(prev => ++prev);
+
       } else {
         copy[currentIndex] = {
-          className: copy[currentIndex].letter === " " ? "wrong-space" : "wrong",
+          className: copy[currentIndex].letter === " " ?
+            "wrong-space" :
+            "wrong",
           letter: copy[currentIndex].letter
         };
       }
 
       return copy;
     });
+
     ++currentIndex
 
   }
 
   useEffect(() => {
 
-    window.addEventListener("keyup", (e) => handleClickKey(e));
+    window.addEventListener("keyup", handleClickKey);
 
     return () => {
-      window.removeEventListener("keyup", (e) => handleClickKey(e));
+      window.removeEventListener("keyup", handleClickKey);
     }
-
   }, []);
+
+  const pixelMoves = Math.floor(700 / lettersList.length);
 
   return (
     <Fragment>
 
       <div className="main">
         <h3>{correct} / {lettersList.length}</h3>
+
+        <div>
+          <img
+            width={80}
+            height={50}
+            src={carSrc}
+            alt="car"
+            style={{
+              transform: `translateX(${correct * pixelMoves}px)`,
+              transition: "all 0.5s ease"
+            }}
+          />
+          <div className="wall" />
+        </div>
+
         <div className="container">
           {lettersList.map((item, index) => (
             <div
@@ -73,7 +97,10 @@ export const TypeRacer = () => {
               className={`
               letter 
               ${item.className} 
-              ${index === currentIndex + 1 ? " active" : ""}`}
+              ${index === currentIndex + 1 ?
+                  " active" :
+                  ""}`
+              }
             >
               {
                 item.letter === " " ?
@@ -85,6 +112,7 @@ export const TypeRacer = () => {
         </div>
 
       </div>
+
       <Alert
         open={open}
         onClose={() => setOpen(false)}
